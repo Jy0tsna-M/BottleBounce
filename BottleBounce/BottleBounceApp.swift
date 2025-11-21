57f91033b7cec613ebcdbff29ff7e0ddc4a4aa99
Jy0tsna <sai.jyotsna@enmovil.in>
2025-11-21 23:02:58 +0530

    Initial Commit

---
+++ BottleBounce/BottleBounceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottleBounceApp.swift
+//  BottleBounce
+//
+//  Created by SaiJyotsna on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BottleBounceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
